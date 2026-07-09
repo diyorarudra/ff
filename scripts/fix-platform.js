a649223ac38cb350af22b726f0ab5deaee840f98
Tarang Vaghani <tarangvaghani@gmail.com>
2026-07-09 23:33:04 +0530

    Clean up unused QA and patch scripts

--- scripts/fix-platform.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-const file = path.join(__dirname, '../js/platform.js');
-let content = fs.readFileSync(file, 'utf8');
-content = content.replace(/\\\`/g, '\`').replace(/\\\$/g, '$');
-fs.writeFileSync(file, content, 'utf8');
-console.log('Fixed platform.js backticks!');
